3ea5708b57b6878fa12f01df37aaebfbc7df93ce
GabrielMerenciano <gabrielmerenciano@hotmail.com>
2025-06-10 15:02:25 -0300

    Create prodcons.tsx

---
+++ entrega-final/app/routes/prodcons.tsx
@@ -0,0 +1,8 @@
+export default function Prodcons() {
+  return (
+    <div>
+      <h1>Produtor-Consumidor</h1>
+      <p>Simulador interativo do problema clássico com controle dinâmico de produtores, consumidores e tamanho do buffer.</p>
+    </div>
+  );
+}
