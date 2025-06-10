e4ec6d0c50299f3a520d040d19953ab7a7fc4868
GabrielMerenciano <gabrielmerenciano@hotmail.com>
2025-06-10 15:01:14 -0300

    Create processes.tsx

---
+++ entrega-final/app/routes/processes.tsx
@@ -0,0 +1,8 @@
+export default function Processes() {
+  return (
+    <div>
+      <h1>Processos CPU-bound e I/O-bound</h1>
+      <p>Visualização e execução de processos com diferentes comportamentos de uso de recursos.</p>
+    </div>
+  );
+}
